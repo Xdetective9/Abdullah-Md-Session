9f9c82113904bb0179091dff2f551776ba53e7eb
Xdetective9 <158455774+Xdetective9@users.noreply.github.com>
2025-08-25 14:27:34 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
                     const credsPath = path.join(tempDir, 'creds.json');
                     const sessionData = fs.readFileSync(credsPath, 'utf8');
                     const base64 = Buffer.from(sessionData).toString('base64');
-                    const sessionId = "X-detective~" + base64;
+                    const sessionId = "Abdullah-Md~" + base64;
 
                     await sock.sendMessage(sock.user.id, { text: sessionId });
 
